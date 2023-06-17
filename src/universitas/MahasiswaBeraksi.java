5917942d02f0b6a437688a79296aed8810704e59
Dayattt20 <dayathero20@gmail.com>
2023-06-17 18:39:39 +0800

    Menjalankan program

--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package universitas;
 
-/**
- *
- * @author fazar
- */
 public class MahasiswaBeraksi {
     
+public static void main(String[] args){
+            
+Mahasiswa datadiri = new Mahasiswa("2110010517", "Muhammad Miftah Hidayat", " Teknik Informatika");
+       
+    System.out.println("Nama    : "+datadiri.getNama());
+    System.out.println("NPM     : "+datadiri.getNpm());
+    System.out.println("Prodi   : "+datadiri.getProdi());
+        
+    System.out.println(datadiri.setDetail());
+}
 }
