13796ba4e54f24d72e2409ffa384017ecea00ca1
Dayattt20 <dayathero20@gmail.com>
2023-06-17 18:32:52 +0800

    Melengkapi class mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -1,5 +1,43 @@
 package universitas;
 
 public class Mahasiswa {
-String nama,npm,prodi;    
+
+  private String nama;
+  private String npm;
+  private String prodi;
+
+    public Mahasiswa(String npm, String nama, String prodi) {
+        this.nama = nama;
+        this.npm = npm;
+        this.prodi = prodi;
+    }
+  
+    public String setDetail(){
+        return npm + nama + prodi;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public String getNpm() {
+        return npm;
+    }
+    
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
 }
+
